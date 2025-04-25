45e98cbda044438152e4ed4662ca609e0246e3ff
deakodev <deakodev@outlook.com>
2025-04-25 13:17:57 -0400

    Add exit command to repl

--- repl.go
+++ repl.go
@@ -25,10 +25,43 @@ func replRun() {
 		}
 
 		cleanText := cleanInput(text)
-		fmt.Printf("Command: %s\n", cleanText[0])
+		command := cleanText[0]
+		fmt.Printf("Command: %s\n", command)
+
+		handler := commandMap[command].callback
+		if handler == nil {
+			fmt.Printf("command handler unknown.")
+			return
+		}
+
+		err := handler()
+		if err != nil {
+			fmt.Printf("command handler error: %v", err)
+			return
+		}
 	}
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	fmt.Printf("Exiting remedy...")
+	os.Exit(0)
+	return nil
+}
+
+type command struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandMap = map[string]command{
+	"exit": {
+		name:        "exit",
+		description: "Exit remedy.",
+		callback:    commandExit,
+	},
 }
